b2153bbf7ef4dc7a7f079aec9b0c042765394f43
jahanvi kumari <95964935+sharmajhanvi@users.noreply.github.com>
2021-12-25 12:38:20 +0530

    Day 1
    I have created a band name generator using Python Code.

---
+++ Day_1_band_name_generator.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 24 11:17:07 2021
+
+@author: Jahanvi Kumari
+"""
+print("welcom to the band name genration.")
+print("what name of the city grew up in?")
+city_num=input()
+print("what's your pet's name?")
+pet_num=input()
+print("your band name could be "+city_num+" " +pet_num)
